d2b34bd187c53ed7e7a9f3afa726b80bf7934375
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-09-14 16:40:55 +0900

    Fix settings page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -9,26 +9,22 @@ const Settings: NextPage = () => {
   return (
     <Nav category="settings">
       <Head>
-        <title>e-Shoku</title>
+        <title>設定 | e-Shoku</title>
       </Head>
       <div className="container">
-        <h1>Next.js and Auth0 Example</h1>
-
+        <h2>設定</h2>
         {isLoading && <p>Loading login info...</p>}
-
         {error && (
           <>
             <h4>Error</h4>
             <pre>{error.message}</pre>
           </>
         )}
-
         {user && (
           <div>
             Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
           </div>
         )}
-
         {!isLoading && !error && !user && (
           <div>
             <a href="/api/auth/login">Login</a>
