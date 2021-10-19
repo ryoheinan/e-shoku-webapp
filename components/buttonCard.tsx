c1e8d5ff5e86cede29b0b156a5bb1d6355c258f6
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-10-19 22:39:54 +0900

    Support useAnchorOnly in buttonCard

--- components/buttonCard.tsx
+++ components/buttonCard.tsx
@@ -1,6 +1,21 @@
 import Link from 'next/link'
 import styles from './buttonCard.module.scss'
 
+interface Params {
+  title: string
+  color: string
+  fontSize: string
+  shadow: boolean
+  link?: LinkParams
+  linkColor?: string
+  children?: React.ReactNode
+}
+
+interface LinkParams {
+  to: string
+  useAnchorOnly?: boolean
+}
+
 const ButtonCard = ({
   title,
   color,
@@ -9,19 +24,11 @@ const ButtonCard = ({
   link,
   linkColor = '#000000',
   children,
-}: {
-  title: string
-  color: string
-  fontSize: string
-  shadow: boolean
-  link?: string
-  linkColor?: string
-  children?: React.ReactNode
-}) => {
+}: Params) => {
   return (
     <>
-      {link ? (
-        <Link href={link}>
+      {link?.to && !link?.useAnchorOnly && (
+        <Link href={link.to}>
           <a className={styles.link} style={{ color: linkColor }}>
             <div
               className={`rounded ${shadow ? styles.cardShadow : ''}`}
@@ -43,7 +50,30 @@ const ButtonCard = ({
             </div>
           </a>
         </Link>
-      ) : (
+      )}
+      {link?.to && link?.useAnchorOnly && (
+        <a className={styles.link} href={link.to} style={{ color: linkColor }}>
+          <div
+            className={`rounded ${shadow ? styles.cardShadow : ''}`}
+            style={{ backgroundColor: color }}
+          >
+            {children ? (
+              <div
+                className={
+                  'd-flex justify-content-center align-items-center py-3 px-2'
+                }
+                style={{ fontSize: fontSize }}
+              >
+                <div className={'me-1'}>{children}</div>
+                <div>{title}</div>
+              </div>
+            ) : (
+              <div className={''}>{title}</div>
+            )}
+          </div>
+        </a>
+      )}
+      {!Link && (
         <div
           className={`rounded ${shadow ? styles.cardShadow : ''}`}
           style={{ backgroundColor: color }}
