680ef704e6d16e0600ba12a55ce30c99e5040099
shintaro3647 <69247511+shintaro3647@users.noreply.github.com>
2021-10-28 00:17:51 +0000

    Fixes _document.tsx and globals.scss #47

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
       <Html>
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </Head>
         <body>
           <Main />
@@ -18,3 +18,4 @@ export default class MyDocument extends Document {
     )
   }
 }
+export default MyDocument
