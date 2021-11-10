ed0a706824d090107d91fba4669126fc2e6b66d9
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-10 23:55:50 +0900

    Fix loading icon styling

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
+import styles from './loading.module.scss'
 
 const Loading = ({ message = '読み込み中...' }: { message?: string }) => {
   return (
-    <div className="text-center">
-      <Image
-        alt="Loading"
-        src="/images/loading_icon.png"
-        width={80}
-        height={80}
-      />
-      <p>{message}</p>
+    <div className={`text-center ${styles.elementCenter}`}>
+      <div>
+        <Image
+          alt={message}
+          src="/images/loading_icon.png"
+          width={80}
+          height={80}
+        />
+        <p>{message}</p>
+      </div>
     </div>
   )
 }
