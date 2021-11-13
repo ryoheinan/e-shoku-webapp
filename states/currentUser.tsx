05616ce294b627868317c12ed761e68cec650685
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-14 00:10:26 +0900

    Add comments

--- states/currentUser.tsx
+++ states/currentUser.tsx
@@ -1,6 +1,9 @@
 import { UserData } from '../types/UserInfo'
 import { atom } from 'recoil'
 
+/**
+ * ユーザー情報の状態保存（Recoil）
+ */
 export const currentUserState = atom<undefined | null | UserData>({
   key: 'CurrentUser',
   default: undefined,
