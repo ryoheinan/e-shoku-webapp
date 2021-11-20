3e22f9163b198ab15cabaaa1d00731da0dcb8665
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-20 11:53:53 +0900

    Implement room BFF (API Routes)

--- pages/api/room/[id].ts
+++ pages/api/room/[id].ts
@@ -1,9 +1,12 @@
 import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0'
-import { roomApiController } from '../../utils/roomApiController'
+import { roomApiController } from '../../../utils/roomApiController'
 
 export default withApiAuthRequired(async function user(req, res) {
   const { accessToken } = await getAccessToken(req, res, {
     scopes: ['openid', 'profile'],
   })
-  roomApiController({ req, res, accessToken })
+  const { id } = req.query
+  if (typeof id === 'string') {
+    roomApiController({ req, res, accessToken, id })
+  }
 })
