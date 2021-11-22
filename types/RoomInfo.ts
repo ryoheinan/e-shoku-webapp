a45e150bcd6aab9c8f43fa44d024d08bc1830688
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-23 01:56:46 +0900

    Update RoomInfo.ts - Create RoomDataCommon - Add new fileds to RoomData

--- types/RoomInfo.ts
+++ types/RoomInfo.ts
@@ -1,24 +1,31 @@
 import { UserData } from './UserInfo'
 
 /**
- * ルーム情報のフォームの型
+ * ルーム情報の共通型
  */
-export interface RoomForm {
+export interface RoomDataCommon {
   room_name: string
   description: string
-  date: string
-  time: string
   topic?: string // 後日実装
   inviteCode?: string // 後日実装
 }
 
 /**
- * ルーム情報の型
+ * ルーム情報のフォームの型
  */
-export interface RoomData extends RoomForm {
+export interface RoomForm extends RoomDataCommon {
+  date: string
+  time: string
+}
+
+/**
+ * ルーム情報をbackendとやりとりするための型
+ */
+export interface RoomData extends RoomDataCommon {
   id: string
   hosts?: UserData[]
   guests?: UserData[]
   created_at: string
-  // capacity: number
+  capacity: number
+  datetime: string
 }
