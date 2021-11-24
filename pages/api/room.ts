81f9f598aa6524ebd536c5e72de918e23e4217ed
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-24 23:43:48 +0900

    Delete room.ts

--- pages/api/room.ts
+++
@@ -1,9 +0,0 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0'
-import { roomApiController } from '../../utils/roomApiController'
-
-export default withApiAuthRequired(async function user(req, res) {
-  const { accessToken } = await getAccessToken(req, res, {
-    scopes: ['openid', 'profile'],
-  })
-  roomApiController({ req, res, accessToken })
-})
