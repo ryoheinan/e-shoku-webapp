885d16046dea3463ecd90e143f131853a5a7c5a1
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-27 17:05:40 +0900

    Fix next image settings

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ module.exports = {
   target: 'serverless',
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: [
+      'storage.googleapis.com',
+      'lh1.googleusercontent.com',
+      'lh2.googleusercontent.com',
+      'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
+    ],
   },
 }
