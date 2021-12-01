f907c520e1132349df46effe28cfba95af57175c
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-12-01 22:17:50 +0900

    Restore 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,49 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Error404: NextPage = () => {
+  return (
+    <div style={styles.error}>
+      <Nav>
+        <Head>
+          <title>404 Not found | e-Shoku</title>
+        </Head>
+
+        <section className="text-center mt-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="72px"
+            viewBox="0 0 24 24"
+            width="72px"
+            fill="#e05d5d"
+          >
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
+          </svg>
+
+          <h2>
+            404
+            <br />
+            Not found
+          </h2>
+          <p className="text-center">お探しのページは見つかりませんでした</p>
+        </section>
+      </Nav>
+    </div>
+  )
+}
+
+export default Error404
+
+const styles: { [k: string]: React.CSSProperties } = {
+  error: {
+    color: '#000',
+    background: '#fff',
+    height: '100vh',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}
