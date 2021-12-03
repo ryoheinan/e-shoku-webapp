5c61ac0d1e2c5df14d088cd334865398e78fac89
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-12-03 21:59:57 +0900

    Add a link to profile page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import { useUser } from '@auth0/nextjs-auth0'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import Nav from '../components/nav'
 import ButtonCard from '../components/buttonCard'
@@ -16,7 +15,7 @@ const Settings: NextPage = () => {
   return (
     <Nav category="settings">
       <Head>
-        <title>プロフィール | e-Shoku</title>
+        <title>設定 | e-Shoku</title>
       </Head>
       <div className="container">
         {isLoading && (
@@ -42,7 +41,7 @@ const Settings: NextPage = () => {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="25px"
-                    viewBox="2 2 24 24"
+                    viewBox="0 0 25 25"
                     width="25px"
                     fill="#000000"
                   >
@@ -53,12 +52,40 @@ const Settings: NextPage = () => {
               </Link>
             </div>
             <div className={styles.settingsItem}>
+              <Link href={`/user/${currentUser.id}`}>
+                <a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    enableBackground="new 0 0 24 24"
+                    height="25px"
+                    viewBox="0 0 25 25"
+                    width="25px"
+                    fill="#000000"
+                  >
+                    <g>
+                      <rect fill="none" height="24" width="24" y="0" />
+                    </g>
+                    <g>
+                      <g>
+                        <rect height="1.5" width="4" x="14" y="12" />
+                        <rect height="1.5" width="4" x="14" y="15" />
+                        <path d="M20,7h-5V4c0-1.1-0.9-2-2-2h-2C9.9,2,9,2.9,9,4v3H4C2.9,7,2,7.9,2,9v11c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V9 C22,7.9,21.1,7,20,7z M11,7V4h2v3v2h-2V7z M20,20H4V9h5c0,1.1,0.9,2,2,2h2c1.1,0,2-0.9,2-2h5V20z" />
+                        <circle cx="9" cy="13.5" r="1.5" />
+                        <path d="M11.08,16.18C10.44,15.9,9.74,15.75,9,15.75s-1.44,0.15-2.08,0.43C6.36,16.42,6,16.96,6,17.57V18h6v-0.43 C12,16.96,11.64,16.42,11.08,16.18z" />
+                      </g>
+                    </g>
+                  </svg>{' '}
+                  マイプロフィール
+                </a>
+              </Link>
+            </div>
+            <div className={styles.settingsItem}>
               <Link href="#">
                 <a>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="25px"
-                    viewBox="2 2 24 24"
+                    viewBox="0 0 25 25"
                     width="25px"
                     fill="#000000"
                   >
@@ -76,12 +103,14 @@ const Settings: NextPage = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 25 25"
                   height="25px"
-                  viewBox="2 2 24 24"
+                  viewBox="0 0 25 25"
                   width="25px"
                   fill="#000000"
                 >
-                  <path d="M 19.5 6 c -1.31 0 -2.37 1.01 -2.48 2.3 C 15.14 7.8 14.18 6.5 12 6.5 c -2.19 0 -3.14 1.3 -5.02 1.8 C 6.87 7.02 5.81 6 4.5 6 C 3.12 6 2 7.12 2 8.5 V 9 c 0 6 3.6 7.81 6.52 7.98 C 9.53 17.62 10.72 18 12 18 s 2.47 -0.38 3.48 -1.02 C 18.4 16.81 22 15 22 9 V 8.5 C 22 7.12 20.88 6 19.5 6 Z M 3.5 9 V 8.5 c 0 -0.55 0.45 -1 1 -1 s 1 0.45 1 1 v 3 c 0 1.28 0.38 2.47 1.01 3.48 C 4.99 14.27 3.5 12.65 3.5 9 Z M 20.5 9 c 0 3.65 -1.49 5.27 -3.01 5.98 c 0.64 -1.01 1.01 -2.2 1.01 -3.48 v -3 c 0 -0.55 0.45 -1 1 -1 s 1 0.45 1 1 V 9 Z M 10.69 10.48 c -0.44 0.26 -0.96 0.56 -1.69 0.76 V 10.2 c 0.48 -0.17 0.84 -0.38 1.18 -0.58 C 10.72 9.3 11.23 9 12 9 s 1.27 0.3 1.8 0.62 c 0.34 0.2 0.71 0.42 1.2 0.59 v 1.04 c -0.75 -0.21 -1.26 -0.51 -1.71 -0.78 C 12.83 10.2 12.49 10 12 10 C 11.51 10 11.16 10.2 10.69 10.48 Z" />
+                  <rect fill="none" height="24" width="24" />
+                  <path d="M19.5,6c-1.31,0-2.37,1.01-2.48,2.3C15.14,7.8,14.18,6.5,12,6.5c-2.19,0-3.14,1.3-5.02,1.8C6.87,7.02,5.81,6,4.5,6 C3.12,6,2,7.12,2,8.5V9c0,6,3.6,7.81,6.52,7.98C9.53,17.62,10.72,18,12,18s2.47-0.38,3.48-1.02C18.4,16.81,22,15,22,9V8.5 C22,7.12,20.88,6,19.5,6z M3.5,9V8.5c0-0.55,0.45-1,1-1s1,0.45,1,1v3c0,1.28,0.38,2.47,1.01,3.48C4.99,14.27,3.5,12.65,3.5,9z M7,11.5V9.85c1.12-0.23,1.95-0.69,2.66-1.08C10.48,8.33,11.07,8,12,8c0.93,0,1.52,0.33,2.34,0.78c0.71,0.39,1.54,0.84,2.66,1.08 v1.65c0,2.76-2.24,5-5,5S7,14.26,7,11.5z M20.5,9c0,3.65-1.49,5.27-3.01,5.98c0.64-1.01,1.01-2.2,1.01-3.48v-3c0-0.55,0.45-1,1-1 s1,0.45,1,1V9z M10.69,10.48c-0.44,0.26-0.96,0.56-1.69,0.76V10.2c0.48-0.17,0.84-0.38,1.18-0.58C10.72,9.3,11.23,9,12,9 s1.27,0.3,1.8,0.62c0.34,0.2,0.71,0.42,1.2,0.59v1.04c-0.75-0.21-1.26-0.51-1.71-0.78C12.83,10.2,12.49,10,12,10 C11.51,10,11.16,10.2,10.69,10.48z" />
                 </svg>{' '}
                 COVID-19について
               </a>
@@ -91,7 +120,7 @@ const Settings: NextPage = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="25px"
-                  viewBox="2 2 24 24"
+                  viewBox="0 0 25 25"
                   width="25px"
                   fill="#000000"
                 >
