c083fc4f02f8ee73521342eabf6243cdc3a5259f
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-12-06 00:39:12 +0900

    Fix initialization process Fixes #84

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
 import { UserData } from '../types/UserInfo'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useSetRecoilState, RecoilRoot } from 'recoil'
-import { currentUserState } from '../states/currentUser'
+import { useRouter } from 'next/router'
 import { UserProvider } from '@auth0/nextjs-auth0'
 import axios from 'axios'
+import { currentUserState } from '../states/currentUser'
 import '../styles/globals.scss'
-import { useRouter } from 'next/router'
 
 const AppInit = () => {
   const setCurrentUser = useSetRecoilState(currentUserState)
@@ -16,11 +16,12 @@ const AppInit = () => {
     const checkUserInfo = async () => {
       try {
         const res = await axios.get<UserData>('/api/user')
-        const currentUser: UserData = res.data
-        console.log(currentUser)
-        if (currentUser.is_info_filled === false) {
+        const currentUser = res.data
+        if (
+          currentUser.is_info_filled === false &&
+          router.pathname !== '/signup'
+        ) {
           setCurrentUser(null)
-          console.log('user info is not filled')
           router.push('/signup')
         } else {
           setCurrentUser(currentUser)
@@ -30,12 +31,13 @@ const AppInit = () => {
       }
     }
     checkUserInfo()
-  }, [setCurrentUser])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.pathname])
 
   return null
 }
 
-const MyApp = ({ Component, pageProps, router }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <UserProvider>
       <RecoilRoot>
