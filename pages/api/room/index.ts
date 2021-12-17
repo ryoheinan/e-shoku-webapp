1a03823998bb293e0a6cb70457d74db085307503
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-12-18 01:21:32 +0900

    Fix room get api

--- pages/api/room/index.ts
+++ pages/api/room/index.ts
@@ -3,14 +3,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { roomApiController } from '../../../utils/roomApiController'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    await roomApiController({ req, res }) // ここのファイル内だと、withApiAuthRequiredが適用されてログイン必須になる
-  } else {
-    const { accessToken } = await getAccessToken(req, res, {
-      scopes: ['openid', 'profile'],
-    })
-    await roomApiController({ req, res, accessToken })
-  }
+  const { accessToken } = await getAccessToken(req, res, {
+    scopes: ['openid', 'profile'],
+  })
+  await roomApiController({ req, res, accessToken })
 }
 
 export default withApiAuthRequired(handler)
